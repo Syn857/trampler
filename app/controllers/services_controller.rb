62707571532f43e5d334821852a314c2d3f081a8
syn857 <josiahpeter29@gmail.com>
2017-05-23 11:51:23 +0800

    Added the fix for empty blank form

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,6 +4,7 @@ class ServicesController < ApplicationController
   end
 
   def show
+    @service = Service.find(params[:id])
   end
 
   def new
@@ -11,11 +12,23 @@ class ServicesController < ApplicationController
   end
 
   def create
+    @service = Service.new(service_params)
+
+    if @service.save
+      redirect_to @service
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def service_params
+    params.require(:service).permit(:title, :description, :price, :requirements)
   end
 end
